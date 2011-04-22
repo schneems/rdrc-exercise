f7b93574988d70e89cddbc7f480f8c12c35e18a0
Matt Mills <sunrisetimes@gmail.com>
2011-04-22 12:17:27 -0500

    working game

--- blackjack.rb
+++ blackjack.rb
@@ -1,7 +1,7 @@
 Deck = [2,3,4,5,6,7,8,9,10, :jack, :queen, :king, :ace]
 class Game
 
-
+require 'pp'
   attr_accessor :dealer, :players
   def initialize(num_of_players = 1)
     @dealer = Player.new
@@ -11,6 +11,16 @@ class Game
   def play_round
     @dealer.play
     @players.each(&:play)
+    @players = @players.reject{|player| player.hand.score > 21}
+    @players.sort! {|player| player.hand.score}
+    top_player = [@players.last.object_id.to_s.to_sym,  @players.last.hand.score]
+    puts "Top Player Score: #{@players.last.hand.score} id: #{@players.last.object_id}"
+    puts "Dealer Score: #{@dealer.hand.score}"
+    if top_player[1] > @dealer.hand.score
+	    puts "player wins!"
+    else
+      puts "dealer wins."
+    end
   end
 end
 
@@ -26,8 +36,8 @@ class Hand
     cards.each do |card|
       total += card if card.is_a? Integer
       total += 10 if card == :jack || card == :queen || card == :king
-      total += 1 if card == :ace && card + 11 > 21
-      total += 11 if card == :ace && card + 11 < 21
+      total += 1 if card == :ace && total + 11 > 21
+      total += 11 if card == :ace && total + 11 < 21
     end
     return total
   end
@@ -37,7 +47,7 @@ class Hand
   end
 end
 
-require 'pp'
+
 
 class Player
   attr_accessor :hand
@@ -49,15 +59,17 @@ class Player
   end
   
   def play
+    @output.puts "Player #: #{self.object_id}"
     @output.puts "Your Score: #{hand.score}. Hit? y/n" 
     answer = @input.gets.chomp
     if answer.match(/y/i)   
-      game.player_hand.hit
+      @hand.hit
       @output.puts "Score: #{hand.score}"
       if hand.score > 21
         @output.puts "Busted."
       else
-        play
+      
+        self.play
       end
     end
   end
